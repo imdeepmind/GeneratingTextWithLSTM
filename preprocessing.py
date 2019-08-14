b114b8aedaad84a0f794bed6dec1377d6d13c672
imdeepmind <infinityatme@gmail.com>
2019-08-14 22:15:33 +0530

    added batch system for processing data

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from math import ceil
 from imblearn.under_sampling import RandomUnderSampler
 
 from utils import clean_review, character_to_number, CHARS
 
 # Constants
 SEQ_LENGTH = 40
+BATCH_SIZE = 10000
 
 randomSampler = RandomUnderSampler(random_state=1969)
 
@@ -27,67 +29,67 @@ data = data[['review_body']]
 # Dropping rows with nan values
 data = data.dropna()
 
-# As I have limited RAM(8GB), I'll only work with fraction of the data
-data = data.sample(frac=0.05)
-
-data = data.values
-
-review_df = pd.DataFrame(columns=['sequence', 'next'])
-review_sequence = []
-next_sequence = []
-
-for index, review in enumerate(data):
-    if index % 1000 == 0:
-        print('--Processing {}th review'.format(index))
+for k in range(ceil(len(data) / BATCH_SIZE)):
+    print(k * BATCH_SIZE, k * BATCH_SIZE + BATCH_SIZE)
     
-    review = review[0]
+    batch_data = data.iloc[k * BATCH_SIZE: k * BATCH_SIZE + BATCH_SIZE]
     
-    # Cleaning the reviews
-    review = clean_review(review)
+    batch_data = batch_data.values
+
+    review_df = pd.DataFrame(columns=['sequence', 'next'])
+    review_sequence = []
+    next_sequence = []
     
-    # Generating sequence
-    for i in range(len(review) - SEQ_LENGTH):
-        # Selecting the sequence
-        seq = review[i:SEQ_LENGTH + i]
-        nxt = review[SEQ_LENGTH + i]
+    for index, review in enumerate(batch_data):
+        if index% 1000 == 0:
+            print('--Processing batch {} {}th review'.format(k+1,index))
         
-        # Converting to ascii
-        seq = character_to_number(seq)
-        nxt = character_to_number(nxt)[0]
+        review = review[0]
         
-        # Appending the data
-        review_sequence.append(seq)
-        next_sequence.append(nxt)
-
-del data
-
-review_df['sequence'] = review_sequence
-review_df['next'] = next_sequence
-
-del review_sequence, next_sequence
-
-review_df = review_df[review_df['next'] != CHARS['--']]
-
-review_df = review_df.sample(frac=1)
-
-X, y = randomSampler.fit_resample(review_df['sequence'].values.reshape(-1,1),review_df['next'].values.reshape(-1,1))
-
-del review_df
-
-final_data = []
-
-for i in range(len(X)):
-    temp = X[i][0]
-    temp.append(y[i][0])
-    final_data.append(temp)
-
-# Converting the array into numpy array
-dataset = np.array([final_data])
-
-del final_data
-
-# Reshaping the data
-dataset = dataset.reshape(dataset.shape[1], SEQ_LENGTH + 1)
-
-# Saving the numpy array
-np.save('dataset/sequence', dataset)
+        # Cleaning the reviews
+        review = clean_review(review)
+        
+        # Generating sequence
+        for i in range(len(review) - SEQ_LENGTH):
+            # Selecting the sequence
+            seq = review[i:SEQ_LENGTH + i]
+            nxt = review[SEQ_LENGTH + i]
+            
+            # Converting to ascii
+            seq = character_to_number(seq)
+            nxt = character_to_number(nxt)[0]
+            
+            # Appending the data
+            review_sequence.append(seq)
+            next_sequence.append(nxt)
+    
+    review_df['sequence'] = review_sequence
+    review_df['next'] = next_sequence
+    
+    del review_sequence, next_sequence
+    
+    review_df = review_df[review_df['next'] != CHARS['--']]
+    
+    review_df = review_df.sample(frac=1)
+    
+    X, y = randomSampler.fit_resample(review_df['sequence'].values.reshape(-1,1),review_df['next'].values.reshape(-1,1))
+    
+    del review_df
+    
+    final_data = []
+    
+    for i in range(len(X)):
+        temp = X[i][0]
+        temp.append(y[i][0])
+        final_data.append(temp)
+    
+    # Converting the array into numpy array
+    dataset = np.array([final_data])
+    
+    del final_data
+    
+    # Reshaping the data
+    dataset = dataset.reshape(dataset.shape[1], SEQ_LENGTH + 1)
+    
+    # Saving the numpy array
+    np.save('dataset/sequence_{}'.format(k), dataset)
