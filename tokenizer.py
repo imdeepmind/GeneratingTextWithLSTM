17fb7e70d50b26f7beafe93d47fc62e1ad968ffc
imdeepmind <infinityatme@gmail.com>
2019-08-20 16:07:52 +0530

    added tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+from keras.preprocessing.text import Tokenizer
+
+from constants import NO_CHARS
+from utils import clean_review
+
+tokenizer = Tokenizer(num_words=NO_CHARS)
+
+data = pd.read_csv('dataset/02.tsv', sep='\t', error_bad_lines=False)
+
+data = data[data['verified_purchase'] == 'Y']
+
+data = data[['review_body']]
+
+data = data.dropna()
+
+data = data.values
+
+for index, review in enumerate(data):
+    if index % 1000 == 0:
+        print('Tokenizing review {}'.format(index))
+        
+    review = clean_review(review[0])
+    
+    tokenizer.fit_on_texts(review)
+    
+with open('dataset/tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
