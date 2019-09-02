7fe91b8e430d10baf82fc97c9e58d38109d66817
imdeepmind <infinityatme@gmail.com>
2019-09-02 20:50:30 +0530

    updated sample, now supporting different temps

--- callback.py
+++ callback.py
@@ -3,7 +3,7 @@ import keras
 import numpy as np
 
 # Importing some constants
-from constants import DEMO_REVIEW, PREDICT_CHARS, MAX_LENGTH
+from constants import DEMO_REVIEW, PREDICT_CHARS, MAX_LENGTH, TEMPERATURE
 
 # Class CustomCallback 
 class CustomCallback(keras.callbacks.Callback):
@@ -31,25 +31,28 @@ class CustomCallback(keras.callbacks.Callback):
     
     # This method runs after each epoch
     def on_epoch_end(self, epoch, logs={}):
-        # Selecting first 40 characters and also converting the text into lowercase as the model supports it only
-        review = DEMO_REVIEW.lower()[0:MAX_LENGTH]
-        review = [ord(i) for i in review]
-        
-        # Predicting the next characters for PREDICT_CHARS times
-        for k in range(PREDICT_CHARS):
-            # Here im selecting the first 40 characters and using the one hot method to encode the data
-            x = self.one_hot(review[k: k + MAX_LENGTH])
-            
-            # Predicting using the model
-            temp = self.model.predict(x)
-            
-            # Calling the sample method
-            temp = self.sample(temp[0])
-            
-            # Appending the predicted charcter
-            review.append(temp)
-        
         # Printing some info and predicted text
-        print('Currently at epoch {}'.format(epoch))
+        print('Currently at epoch {}'.format(epoch + 1))
         print('Starter text : {}'.format(DEMO_REVIEW))
-        print('Generated text: {}'.format(''.join([chr(i) for i in review])))
+        
+        for temperature in TEMPERATURE:
+            # Selecting first 40 characters and also converting the text into lowercase as the model supports it only
+            review = DEMO_REVIEW.lower()[0:MAX_LENGTH]
+            review = [ord(i) for i in review]
+            
+            # Predicting the next characters for PREDICT_CHARS times
+            for k in range(PREDICT_CHARS):  
+                # Here im selecting the first 40 characters and using the one hot method to encode the data
+                x = self.one_hot(review[k: k + MAX_LENGTH])
+
+                # Predicting using the model
+                temp = self.model.predict(x)
+
+                # Calling the sample method
+                temp = self.sample(temp[0], temperature)
+
+                # Appending the predicted charcter
+                review.append(temp)
+        
+
+            print('Generated text with temperature {}: {}'.format(temperature, ''.join([chr(i) for i in review])))
--- constants.py
+++ constants.py
@@ -1,9 +1,8 @@
 # Data related constants
 DATABASE="dataset"
-NO_ROWS_TRAIN=10000000 #159855696
 NO_ROWS_VAL=1000000 #19981962
+NO_ROWS_TRAIN=1000000 #159855696
 NO_ROWS_TEST=19981962
-OUTPUT_CLASSES=128
 
 # Model related constants
 GPU=True
@@ -14,6 +13,7 @@ EPOCHS=1000
 # Prediction and demo related connstants
 DEMO_REVIEW="so if you're looking for earphones playi"
 PREDICT_CHARS=500
+TEMPERATURE=[0.2, 0.5, 1.0, 1.2]
 
 # Other
 WEIGHT_FOLDER='weights'
